dfb3b68337951a8de7381e3e102b384dd4e09b66
Selim Sarikaya <selimsarikaya@192.168.0.193>
2025-02-23 17:32:33 +0300

    Day2.py

---
+++ Day2.py
@@ -0,0 +1,74 @@
+# print("Hello"[-5])
+
+# print("12345"[4])
+
+# #String
+# print("123" + "456")
+
+# print(123 + 456)
+
+# print(123,456,789)
+
+# print(123_456_789)
+# #computer ignores the underscores.
+
+# # Float = floating point number
+# print(3.1415)
+
+# print(type("Hello"))
+# print(type(500))
+# print(type(1.291))
+# print(type(True))
+
+# print("123" + "456")
+# print(int("123") + int("456"))
+
+
+#  print("Number of letters in your name: " + str(len(input("Enter your name: "))))
+
+
+# print(5-39)
+# print(2*93)
+# print(8/3)
+# print(41//7)
+# print(3 ** 5)
+
+# print(3 / 3 + 3 - 3 / 3)
+
+# bmi = 70 / (1.90 * 1.90)
+# print((bmi))
+
+# print(round(bmi))
+# print(round(bmi,3))
+
+# score = 0
+
+# #user scores a point
+
+# score += 1
+# print(score)
+
+# print("Your score is " + str(score))
+
+
+#  f-strings
+
+# height = 1.8
+# is_winning = True
+
+# print(f"Your score is = {score}, your height is = {height}, Are you winning? {is_winning}")
+
+# name = input("Please enter your name: ")
+# print(f"your name is : {name} ")
+
+
+ 
+# Todays project : Tip Calculator
+
+print("Welcome to tip calculator!")
+bill = float(input("What was the total bill?  $ "))
+tip = int(input("How much tip(percentage) would you like to give(10,12 or 15)? "))
+amount_of_people = int(input("How many people to split the bill? "))
+bill_with_tip = bill + bill * (tip/100)
+pay = round(((bill_with_tip) / amount_of_people),2)  
+print(f"Each person should pay: ${pay} ")
