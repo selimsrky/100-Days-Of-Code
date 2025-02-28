7c159df4fff42f45b51d9490c6543344fbffd870
Selim Sarikaya <selimsarikaya@192.168.0.193>
2025-02-28 22:08:01 +0300

    Updated Day4.py

---
+++ Day4.py
@@ -0,0 +1,83 @@
+# import random
+# import exercise
+
+# random_integer = random.randint(1,10)
+# print(random_integer)
+
+# print(Day3.my_favourite_number)
+
+# random_float = random.random()*10
+# print(random_float)
+
+# random_float2 = random.uniform(1,10)
+# print(random_float2)
+
+# random_number = random.random()
+# print(random_number)
+# if(random_number < 0.5):
+#     print("Heads")
+# else:  
+#     print("Tails")
+
+# states_of_america = list = ["Delaware","Pennsylvania","New Jersey","Georgia"]
+# states_of_america[1] = "Pencilvania"
+
+# states_of_america.extend(["Angelaland","Jack Baure Landd"])
+
+# print(states_of_america)
+
+
+# WHO WILL PAY THE BILL
+
+# friends = ["Alice","Bob","Charlie","David","Emanuel"]
+
+# random_friend_guess = random.randint(0,4)
+# print(random_friend_guess)
+# print(friends[random_friend_guess])
+
+# print(len(friends))
+
+# fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+# vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+ 
+# dirty_dozen = [fruits, vegetables]
+ 
+# print(dirty_dozen[1][1])
+
+
+
+
+# ROCK PAPER SCISSORS GAME
+
+# your_guess = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors")
+
+# list_game = ["Rock","Paper","Scissors"]
+
+# if your_guess == "0":
+#     your_guess = list_game[0] 
+# if your_guess == "1":
+#     your_guess = list_game[1]
+# if your_guess == "2":
+#     your_guess = list_game[2]
+
+# random_index = random.randint(0,2)
+# computer_guess = list_game[random_index]
+
+# if computer_guess == "Rock" and your_guess == "Paper":
+#     print("You win the game.")
+
+# elif computer_guess == "Rock" and your_guess == "Scissors":
+#     print("You lose the game.")
+# elif computer_guess == "Paper" and your_guess == "Scissors":
+#     print("You win the game.")
+# elif computer_guess == "Paper" and your_guess == "Rock":
+#     print("You lose the game.")
+# elif computer_guess == "Scissors" and your_guess == "Rock":
+#     print("You win the game.")
+# elif computer_guess == "Scissors" and your_guess == "Paper":
+#     print("You lose the game.")
+
+# else:
+#     print("It's draw.")
+
+
